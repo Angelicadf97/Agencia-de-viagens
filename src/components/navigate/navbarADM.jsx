5d9b088be956f4f0a559cc7c9b8b268806e1a7c1
Angélica Flores <99211551+Angelicadf97@users.noreply.github.com>
2022-10-24 22:36:47 -0300

    Update navbarADM.jsx

--- src/components/navigate/navbarADM.jsx
+++ src/components/navigate/navbarADM.jsx
@@ -22,14 +22,14 @@ export default function NavbarADM() {
                             </div>
                             <div class="offcanvas-body">
                                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li class="nav-item"><a class="nav-link" href="../adm/cliente">Clientes</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="../adm/aeroporto">Aeroportos</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="../adm/hospedagem">Hospedagens</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="../adm/companhia">Companhias</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="../adm/pacote">Pacotes</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="../adm/voo">Voos</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="../adm/reserva">Reserva</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="../adm">Sair</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm/cliente">Clientes</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm/aeroporto">Aeroportos</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm/hospedagem">Hospedagens</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm/companhia">Companhias</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm/pacote">Pacotes</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm/voo">Voos</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm/reserva">Reserva</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="./adm">Sair</a></li>
                                 </ul>
                             </div>
                         </div>
